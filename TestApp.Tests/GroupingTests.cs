f1b3ea851228a9776f3fd2234adf7dd79608fc19
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2023-12-03 18:23:56 +0200

    Add project files.

---
+++ TestApp.Tests/GroupingTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class GroupingTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_GroupNumbers_WithEmptyList_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<int> input = new() ;
+
+        // Act
+        string result=Grouping.GroupNumbers(input) ;
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GroupNumbers_WithEvenAndOddNumbers_ShouldReturnGroupedString()
+    {
+        // Arrange
+        List<int> input = new() { 1, 2, 3, 6, 8, 9 };
+        string expected = "Odd numbers: 1, 3, 9\r\nEven numbers: 2, 6, 8";
+
+        // Act
+        string result = Grouping.GroupNumbers(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+    public void Test_GroupNumbers_WithOnlyEvenNumbers_ShouldReturnGroupedString()
+    {
+
+        // Arrange
+        List<int> input = new() { 2, 6, 8 };
+        string expected = "Even numbers: 2, 6, 8";
+
+        // Act
+        string result = Grouping.GroupNumbers(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GroupNumbers_WithOnlyOddNumbers_ShouldReturnGroupedString()
+    {
+        // Arrange
+        List<int> input = new() { 1, 3, 5 };
+        string expected = "Odd numbers: 1, 3, 5";
+
+        // Act
+        string result = Grouping.GroupNumbers(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+    public void Test_GroupNumbers_WithNegativeNumbers_ShouldReturnGroupedString()
+    {
+        // Arrange
+        List<int> input = new() { -1, -2, -3, -6, -8, -9 };
+        string expected = "Odd numbers: -1, -3, -9\r\nEven numbers: -2, -6, -8";
+
+        // Act
+        string result = Grouping.GroupNumbers(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+}
---
+++ TestApp.Tests/OddOccurrencesTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.Tests;
+
+public class OddOccurrencesTests
+{
+    [Test]
+    public void Test_FindOdd_WithEmptyArray_ShouldReturnEmptyString()
+    {
+        //Arrange
+        string[] input=Array.Empty<string>();
+        //Act
+        string result=OddOccurrences.FindOdd(input);
+        //Assert
+        Assert.AreEqual(string.Empty,result);
+    }
+
+    [Test]
+    public void Test_FindOdd_WithNoOddOccurrences_ShouldReturnEmptyString()
+    {
+        // Arrange
+        string[] input = { "Java", "C#", "PHP", "PHP", "JAVA", "C#" };
+
+        // Act
+        string result = OddOccurrences.FindOdd(input);
+
+        // Assert
+        Assert.That(result,Is.EqualTo(string.Empty));
+    }
+
+    [Test]
+    public void Test_FindOdd_WithSingleOddOccurrence_ShouldReturnTheOddWord()
+    {
+        // Arrange
+        string[] input = { "Java"};
+        string expected = "java" ;
+
+        // Act
+        string result = OddOccurrences.FindOdd(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_FindOdd_WithMultipleOddOccurrences_ShouldReturnAllOddWords()
+    {
+        // Arrange
+        string[] input = { "java", "c#", "java", "c", "java" };
+        string expected = "java c# c";
+
+        // Act
+        string result = OddOccurrences.FindOdd(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_FindOdd_WithMixedCaseWords_ShouldBeCaseInsensitive()
+    {
+        // Arrange
+        string[] input = { "Java", "C#", "JAVA", "C", "java" };
+        string expected = "java c# c";
+
+        // Act
+        string result = OddOccurrences.FindOdd(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
